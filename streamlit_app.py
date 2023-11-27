68892c70da75b4626d0d0a5ce5651cafe90df754
sujathao <152117043+sujathao@users.noreply.github.com>
2023-11-27 14:33:30 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,4 @@ streamlit.text('🐔 dosa and Idly')
 streamlit.text('🥗 oatmeal, bread butter, jam and croissants, smoothie')
 streamlit.text('🍞puri, vada');
 
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import pandas
