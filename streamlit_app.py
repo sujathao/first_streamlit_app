8e3c66eeb5e6d47cca53cfcc584b3c415401967a
sujathao <152117043+sujathao@users.noreply.github.com>
2023-11-27 14:34:07 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥗 oatmeal, bread butter, jam and croissants, smoothie')
 streamlit.text('🍞puri, vada');
 
 import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
