8fffd6b7f6293cc560e219475af1fa844928040b
sujathao <152117043+sujathao@users.noreply.github.com>
2023-11-27 14:27:50 +1100

    Update streamlit_app.py
    adding pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ streamlit.title('my parents first diner')
 streamlit.header('BreakFast Menu')
 streamlit.text('🐔 dosa and Idly')
 streamlit.text('🥗 oatmeal, bread butter, jam and croissants, smoothie')
-streamlit.text('🍞puri, vada')
+streamlit.text('🍞puri, vada');
+
+Import Pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
