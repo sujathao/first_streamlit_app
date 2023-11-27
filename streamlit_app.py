0da1fe571aa4cd4d6180f33b8de84364df991d39
sujathao <152117043+sujathao@users.noreply.github.com>
2023-11-27 13:22:19 +1100

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parents first diner')
+streamlit.header('BreakFast Menu')
+streamlit.text('dosa and Idly')
+streamlit.text('oatmeal, bread butter, jam and croissants, smoothie')
+streamlit.text('puri, vada')
